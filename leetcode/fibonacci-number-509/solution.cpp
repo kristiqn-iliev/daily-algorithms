591e330325b4ee16c0994c399d6d9ef6610ce1f1
kristiqn-iliev <kristiqniliev57@gmail.com>
2025-05-30 18:23:37 +0300

    Solve leetcode-509.

---
+++ leetcode/fibonacci-number-509/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 0;
+    int *arr = new int[n + 1];
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i < n + 1; ++i)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    cout << arr[n] << endl;
+    return 0;
+}
